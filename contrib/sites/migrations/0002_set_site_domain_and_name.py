011f252d66c68747f5120262403fdd4e8be980ca
Sardor <sardor@hanbiro.com>
2015-03-17 15:33:51 +0900

    Migration

--- contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -11,8 +11,8 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "news.dig.uz",
-            "name": "dignews"
+            "domain": "mydomain.uz",
+            "name": "mydomain"
         }
     )
 
